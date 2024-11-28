db42c396268e96a405caa859bd37fb9ab3db4c1f
deepakkasera <deepak.kasera143.dk@gmail.com>
2024-11-29 00:57:07 +0530

    Email Service

---
+++ src/main/java/com/example/emailservicenov24/EmailServiceNov24Application.java
@@ -0,0 +1,13 @@
+package com.example.emailservicenov24;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmailServiceNov24Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmailServiceNov24Application.class, args);
+    }
+
+}
